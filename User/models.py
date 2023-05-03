6b696d9e167786b3e83fe235816848149f7af561
unknown <you@example.com>
2023-05-03 14:35:58 -0300

    feat/modelsUser

---
+++ User/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    email = models.EmailField(max_length=120, null=False)
+    password = models.CharField(max_length=120, null=False)
+    is_employee = models.BooleanField(default=False)
