44a529ab809eea68893b4739c64c9c18ae9baba7
petrarca <pedropetrarcar@gmail.com>
2023-05-04 10:42:09 -0300

    feat: Loan model has been added.

---
+++ Loan/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+import datetime as dt
+
+def generate_devolution_date():
+    today = dt.date.today()
+    next_week = today + dt.timedelta(days=7)
+
+    while next_week.weekday() >= 5:
+        next_week = dt.timedelta(days=1)
+    return next_week
+
+
+class Loan(models.Model):
+    user_id = models.ForeignKey(
+        "User.User",
+        on_delete=models.CASCADE,
+        related_name="loaned_for",
+    )
+    copy_id = models.ForeignKey(
+        "Book.Copy",
+        on_delete=models.CASCADEm
+        related_name="copy_loaned",
+    )
+    devolution_date = generate_devolution_date()
+    is_returned = models.BooleanField(default=False)
