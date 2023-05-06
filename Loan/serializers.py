5a644fda401e0ea3d32fbe83151a4411d652a45f
petrarca <pedropetrarcar@gmail.com>
2023-05-06 01:05:17 -0300

    feat: Create Loan has been added.

--- Loan/serializers.py
+++ Loan/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
 from .models import Loan
+from User.models import User
 
 
 class LoanSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user_id = validated_data["user_id"]
+        user = User.objects.get(id=user_id)
+        print(user)
+        if user["is_banned"]:
+            return {"message": "This user is banned"}
+        return Loan.objects.create(**validated_data)
 
     class Meta:
         model = Loan
@@ -13,3 +21,4 @@ class LoanSerializer(serializers.ModelSerializer):
             "devolution_date",
             "is_returned",
         ]
+
--- Loan/urls.py
+++ Loan/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from Loan.views import CreateLoanView, UpdateLoanView
 
 
-urlpatterns = []
+urlpatterns = [
+    path("loan/", CreateLoanView.as_view()),
+    path("loan/update/", UpdateLoanView.as_view())
+]
