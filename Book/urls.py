415697bb89b735ecf086d99f6b36d766005cd28a
petrarca <pedropetrarcar@gmail.com>
2023-05-06 22:29:27 -0300

    feat: copy view is available, and Loan View is now usable.

--- Book/urls.py
+++ Book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BookView, BookDetailView
+from .views import BookView, BookDetailView, CopyCreateView
 
 urlpatterns = [
     path("book/", BookView.as_view()),
     path("book/<int:pk>/", BookDetailView.as_view()),
+    path("copy/", CopyCreateView.as_view())
 ]
--- Loan/serializers.py
+++ Loan/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.views import Response, status
+from rest_framework.validators import ValidationError
 
 from .models import Loan
 from User.models import User
@@ -6,11 +8,9 @@ from User.models import User
 
 class LoanSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
-        user_id = validated_data["user_id"]
-        user = User.objects.get(id=user_id)
-        print(user)
-        if user["is_banned"]:
-            return {"message": "This user is banned"}
+        user_verify = User.objects.get(username=validated_data["user_id"])
+        if user_verify.is_banned:
+            raise ValidationError({"message": "This user is banned"})
         return Loan.objects.create(**validated_data)
 
     class Meta:
