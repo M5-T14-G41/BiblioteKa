71544865da76d3aacfb794c97be41ad807aa128a
pethe <pethersonreis@gmail.com>
2023-05-09 16:00:02 -0300

    feat/e-mailing

--- Loan/views.py
+++ Loan/views.py
@@ -1,7 +1,10 @@
+from django.forms import model_to_dict
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from Book.models import Copy
+from Book.tasks import send_notification
 from User.models import User
 from User.permissions import IsAdminOrReadOnly, IsAdmin
 from Loan.serializers import LoanSerializer
@@ -35,6 +38,19 @@ class UpdateLoanView(RetrieveUpdateAPIView):
         user_id = get_object_or_404(Loan, id=loan_id).user_id.id
 
         loan = get_object_or_404(Loan, id=loan_id)
+
+        copy = get_object_or_404(Copy, id=loan.copy_id.id)
+        copy.isBorrowed = False
+        copy.save()
+
+        book = model_to_dict(copy.book)
+
+        copies = [model_to_dict(c) for c in Copy.objects.filter(
+            book=book["id"], isBorrowed=False)]
+
+        for user in book["following"]:
+            if copies.__len__() > 0:
+                send_notification(user, copy.book, "isavaliable")
 
         user = get_object_or_404(User, id=user_id)
 
